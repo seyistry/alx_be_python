98889dbfd0e8dc240ff6947bb4fcd917462c768a
Oluwaseyi <seyistry@gmail.com>
2024-06-22 17:45:55 +0100

    add future age script

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,7 @@
+# User Input Age Calculator 
+
+user_input = input('How old are you? ')
+
+age_at__expected_year = int(user_input) + (27)
+
+print('In 2050, you will be {} years old.'.format(age_at__expected_year))
