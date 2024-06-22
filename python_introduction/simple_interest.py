3a7f813d5a5b9e6870b63b55620a3e0e1ef61f9b
Oluwaseyi <seyistry@gmail.com>
2024-06-22 17:13:35 +0100

    refactor interest formula

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -3,5 +3,6 @@
 principal = 1000
 rate = 0.05
 time = 3
+interest = principal * time * rate
 
-print('The simple interest is: {:.1f}'.format(principal * time * rate))
+print('The simple interest is: {:.1f}'.format(interest))
