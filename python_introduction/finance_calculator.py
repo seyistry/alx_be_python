be8550218309f66b564bd831e6d1424bed203383
Oluwaseyi <seyistry@gmail.com>
2024-06-23 11:14:27 +0100

    change type casting from int to float

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -2,9 +2,9 @@
 
 monthly_income = input("Enter your monthly income: ")
 tx_monthly_expenses = input("Enter your total monthly expenses: ")
-monthly_savings = int(monthly_income) - int(tx_monthly_expenses)
+monthly_savings = float(monthly_income) - float(tx_monthly_expenses)
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
 print("Your monthly savings are ${}.".format(monthly_savings))
-print("Projected savings after one year, with interest, is: ${:.0f}.".format(
+print("Projected savings after one year, with interest, is: ${}.".format(
     projected_savings))
