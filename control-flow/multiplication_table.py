d92e45cc70ad9234251498c919d5fe66ca0acdf9
Oluwaseyi <seyistry@gmail.com>
2024-06-29 17:37:45 +0100

    change string format

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -3,4 +3,4 @@
 number = int(input('Enter a number to see its multiplication table: '))
 
 for i in range(1, 11):
-    print('{} * {} = {}'.format(number, i, number * i))
+    print(f'{number} * {i} = {number * i}')
