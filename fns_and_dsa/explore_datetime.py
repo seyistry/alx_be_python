b38e623905b490ddbb30fe38024cbbc91d8d5c06
Oluwaseyi Egunjobi <seyistry@gmail.com>
2024-07-07 17:45:59 +0100

    script that work with python time format

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,14 @@
+from datetime import datetime, timedelta
+
+
+def display_current_datetime():
+    current_date = datetime.now()
+    print(f"Current date and time: {current_date.isoformat(' ', 'seconds')}")
+
+
+def calculate_future_date():
+    future_date = int(
+        input('Enter the number of days to add to the current date: '))
+    ft = datetime.now() + timedelta(days=future_date)
+    print(f'Future date: {ft.strftime("%Y-%m-%d")}')
+
