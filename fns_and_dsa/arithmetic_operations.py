dc8907d9a281e1c0c4fd8aa037e0d6e9f28a3a51
Oluwaseyi Egunjobi <seyistry@gmail.com>
2024-07-07 16:24:48 +0100

    remove bracket from param

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,7 +7,7 @@ def perform_operation(num1, num2, operation):
         case 'multiply':
             return num1 * num2
         case 'divide':
-            if (num2 == 0):
+            if num2 == 0:
                 return 'Can\'t divide by zero'
-            elif (num2 == 0):
+            elif num2 == 0:
                 return num1 / num2
