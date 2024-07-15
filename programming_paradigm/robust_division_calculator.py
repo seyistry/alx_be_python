3180049b4523b41a3b775882f899d53d94e164d8
Oluwaseyi Egunjobi <seyistry@gmail.com>
2024-07-15 21:03:20 +0100

    change input to float

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -2,7 +2,7 @@
 
 def safe_divide(numerator, denominator):
     try:
-        result = int(numerator)/int(denominator)
+        result = float(numerator)/float(denominator)
     except ZeroDivisionError:
         return 'Error: Cannot divide by zero.'
     except ValueError:
