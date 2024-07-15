35a82a4fe1ad46b935fc07715c638b9e52f96a1b
Oluwaseyi Egunjobi <seyistry@gmail.com>
2024-07-15 23:41:59 +0100

    refactor exercise

--- oop/library_system.py
+++ oop/library_system.py
@@ -5,17 +5,26 @@ class Book:
         self.title = title
         self.author = author
 
+    def __str__(self):
+        return f'{Book.__name__}: {self.title} by {self.author}'
+
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size
 
+    def __str__(self):
+        return f'{EBook.__name__}: {self.title} by {self.author}'
+
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
+
+    def __str__(self):
+        return f'{PrintBook.__name__}: {self.title} by {self.author}'
 
 
 class Library:
@@ -26,4 +35,4 @@ class Library:
 
     def list_books(self):
         for book in self.books:
-            print(f'{book.__class__.__name__}: {book.title} by {book.author}')
+            print(book)
